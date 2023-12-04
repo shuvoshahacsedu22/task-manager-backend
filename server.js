fdc3c8740eab793118d7f324b9bd11b7338f01a4
Shuvo Shaha <shuvoshaha7@gmail.com>
2023-12-04 21:52:33 +0600

    git ignore

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,13 +7,14 @@ app.use(bodyParser.json());
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 const TaskRoute = require('./routes/Task');
-
+const port = process.env.PORT || 3000;
 mongoose.Promise = global.Promise;
 
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
     console.log("Databse Connected Successfully!!");    
+    console.log(process.env);
 }).catch(err => {
     console.log('Could not connect to the database', err);
     process.exit();
@@ -21,6 +23,7 @@ mongoose.connect(dbConfig.url, {
 app.use('/task',TaskRoute)
 app.get('/', (req, res) => {
     res.json({"message": "Hello Crud Node Express"});
-});app.listen(3000, () => {
+});
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
