15096caaa5e7ebb8bf5eba2fba1a7e8eabd8ce2c
Shuvo Shaha <shuvoshaha7@gmail.com>
2023-12-06 18:14:10 +0600

    typo

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ mongoose.connect(dbConfig.url, {
 app.use(function(req, res) {
     res.header("Access-Control-Allow-Origin", "https://task-manager-pi-nine.vercel.app/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
   });
 
 app.use('/task',TaskRoute)
